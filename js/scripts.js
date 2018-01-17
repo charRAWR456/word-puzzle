bbeab74c5ff65f8a280ab5dd6f45d692f451f378
Jamie Pittak and Tyler Wickline <tyler@>
2018-01-17 10:32:44 -0800

    Adds working replacement scripts and color

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   //newString = [];
   //checkString = $(".sentance").text();
-  var originalString = $(".sentance").text();
+  originalArray = $(".sentance").text().split("");
   var vowelString = ["a", "e", "i", "o", "u", "y"];
   stringArray = $(".sentance").text().split("");
   for (var i = 0; i < stringArray.length; i++) {
@@ -13,7 +13,7 @@ $(document).ready(function() {
       }
     }
   }
-  var modString = stringArray.join("")
+  modString = stringArray.join("")
   console.log(modString);
   //$(".hidden-sentance").text(newString);
 
@@ -22,18 +22,23 @@ $(document).ready(function() {
   $(".sentance").click(function() {
     $(".sentance").hide();
     $(".modified-string").show();
-  })
+  });
   $(".modified-string").click(function() {
     $(".sentance").toggle();
     $(".modified-string").toggle();
+  });
 
 
 
     $("#wordPuzzle").submit(function(event) {
       event.preventDefault();
-
-
-
+      var userInput = $("input#puzzle").val();
+      for (var i = 0; i < originalArray.length; i++) {
+        if (userInput == originalArray[i]) {
+          stringArray[i] = userInput;
+        }
+      };
+      modString = stringArray.join("")
+      $(".modified-string").text(modString);
     });
-  });
 });
